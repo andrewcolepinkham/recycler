f186452cd39b708b226b53483ffe2c230fd07d2c
andrewcolepinkham <57509695+andrewcolepinkham@users.noreply.github.com>
2021-12-02 18:41:15 -0700

    delete userdata app

--- recyclemanager/userdata/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from userdata.models import User 
-
-# User Serializer
-class UserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User 
-    fields = '__all__'
--- recyclemanager/userdata/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .api import UserViewSet
-
-router = routers.DefaultRouter()
-router.register('api/userdata', UserViewSet, 'userdata')
-
-urlpatterns = router.urls
