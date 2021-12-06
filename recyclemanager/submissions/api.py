4790eafe2f807d0fba91cb8d25d58db2de5d3297
Montgomery <m_abbott@coloradocollege.edu>
2021-12-06 08:09:31 -0700

    integration

--- recyclemanager/submissions/api.py
+++ recyclemanager/submissions/api.py
@@ -2,6 +2,8 @@ from submissions.models import Submission
 from rest_framework import viewsets, permissions
 from .serializers import SubmissionSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
+#accounts.models import Account;
+ 
 
 class SubmissionViewSet(viewsets.ModelViewSet):
     parser_classes = (MultiPartParser, FormParser)
@@ -11,10 +13,12 @@ class SubmissionViewSet(viewsets.ModelViewSet):
     ]
     serializer_class = SubmissionSerializer
     def get_queryset(self):
-
         return self.request.user.submissions.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+        #Account.objects.get(id=self.request.user.id)
+        #print(Account.username)
+
 
     
